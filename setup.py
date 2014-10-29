86e7f84ee02d34967d10c30d9cd1798730bef3da
Stephan Rave <stephanrave@uni-muenster.de>
2014-10-29 18:25:15 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simdb',
-    version='0.1.dev3',
+    version='0.2.dev1',
     packages=find_packages(),
     description='A simple Python package for management of simulation datasets',
     #long_description=long_description,
